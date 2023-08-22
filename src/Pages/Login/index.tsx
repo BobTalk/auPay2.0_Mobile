bc44fa8e724c26c12a0141175be7f21b61ebaba2
noahZhu <noah.zhu@austintechno.com>
2023-08-22 18:18:35 +0800

    移动端 登陆、注册、验证表单、发送验证码。

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,5 +1,7 @@
+import { useRef, useState, useEffect } from 'react'
 import PublicHead from '@/Components/PublicHead'
 import Logo from '@/Assets/images/logo.png'
+import CloseImg from '@/Assets/images/close.png'
 import {
   Form,
   Input,
@@ -7,39 +9,70 @@ import {
 } from 'antd-mobile'
 import './index.scss'
 const Login = () => {
-  const headData = {
-    title: 'auPay用户登录',
-    back: '',
+  const headData = { title: 'auPay用户登录', back: '' }
+  let codeTimer: any = null
+  const [ codeData, setCodeData ] = useState({ timer: null, status: false, time: 60 })
+  const formRef: any = useRef(null)
+  const closePassword = () => {
+    formRef && formRef.current && formRef.current.setFieldValue('password', '')
+  }
+  const getEmailCode = () => { codeTimer = setInterval(codeTime, 1000) }
+  const codeTime = () => { setCodeData(codeData => ({ ...codeData, status: true, time: codeData.time - 1 })) }
+  useEffect(() => {
+    if (codeData.time < 1) return closeCodeTime()
+  }, [codeData])
+  useEffect(() => { return () => { closeCodeTime() } }, [])
+  const closeCodeTime = () => {
+    clearInterval(codeTimer)
+    setCodeData({
+      timer: null,
+      status: false,
+      time: 60
+    })
+  }
+  const onFinish = (value: any) => {
+    console.log('登陆提交数据' + value)
+  }
+  const forget = () => {
+    console.log('忘记密码')
   }
   return (
     <div className='login_wrap w'>
       <PublicHead { ...headData } />
       <img className='logo' src={ Logo } alt="" />
-      <Form className='login_form' layout='horizontal'>
-        <Form.Item
-          name='name'
-          rules={[{ required: true, message: '用户名不能为空' }]}
-        >
+      <Form className='login_form' layout='horizontal' onFinish={onFinish} ref={ formRef }>
+        <Form.Item>
           <p className='login_form_label'>用户名</p>
-          <Input className='login_form_input' placeholder='请输入姓名' />
+          <Form.Item noStyle name='username' rules={[{ required: true, message: '用户名不能为空' }]}>
+            <Input className='login_form_input' placeholder='请输入姓名' />
+          </Form.Item>
         </Form.Item>
 
-        <Form.Item
-          name='password'
-          rules={[{ required: true, message: '登陆密码不能为空' }]}
-        >
+        <Form.Item>
           <p className='login_form_label'>登陆密码</p>
-          <Input type='password' className='login_form_input' placeholder='请输入登陆密码' />
+          <Form.Item noStyle name='password' rules={[{ required: true, message: '登陆密码不能为空' }]}>
+            <Input type='password' className='login_form_input' placeholder='请输入登陆密码' />
+          </Form.Item>
+          <img onClick={ closePassword } className='login_form_close' src={ CloseImg } alt="" />
         </Form.Item>
 
-        <Form.Item
-          name='code'
-          rules={[{ required: true, message: '邮箱验证码不能为空' }]}
-        >
+        <Form.Item>
           <p className='login_form_label'>邮箱验证码</p>
-          <Input type='password' className='login_form_input' placeholder='请输入邮箱验证码' />
+          <Form.Item noStyle name='code' rules={[{ required: true, message: '邮箱验证码不能为空' }]}>
+            <Input className='login_form_input' placeholder='请输入邮箱验证码' />
+          </Form.Item>
+          {
+            codeData.status ? <p className='login_form_get_code'>{ codeData.time } s</p> : <p onClick={ getEmailCode } className='login_form_get_code'>获取验证码</p>
+          }
         </Form.Item>
 
+        <div onClick={ forget } className='login_form_forget'><p>忘记密码</p></div>
+
+        <Form.Item>
+          <Button className='login_form_btn' block type='submit' color='primary'>
+            登录
+          </Button>
+        </Form.Item>
       </Form>
     </div>
   )
--- src/Routers/data.js
+++ src/Routers/data.js
@@ -1,10 +1,12 @@
 import Login from '@/Pages/Login'
+import Register from '@/Pages/Register'
 import ResetLogin from '@/Pages/ResetLogin'
 import { Navigate } from 'react-router-dom'
 
 // 如果需要重定向 咋需要把element换成 <Navigate to={ item.to } />
 const RouteList = [
   { label: '登陆', path: 'login', element: <Login/> },
+  { label: '注册', path: 'register', element: <Register/> },
   { label: '重置密码', path: 'resetLogin', element: <ResetLogin/> },
 ]
 
