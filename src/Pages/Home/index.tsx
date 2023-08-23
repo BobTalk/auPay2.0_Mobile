f215bde915ae1325c59d487cc522730ffc95f461
noahZhu <noah.zhu@austintechno.com>
2023-08-23 18:20:33 +0800

    首页部分

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,35 @@
+import photoImg from '@/Assets/images/test/photo.png'
+import './index.scss'
 
 const Home = () => {
   return (
-    <div>我是首页</div>
+    <div className='home_wrap'>
+
+      <div className="home_banner public_w">
+        <div className='home_banner_info'>
+          <img src={ photoImg } alt="" />
+          <p>西尾猫的世界</p>
+        </div>
+        <ul className='home_banner_assets'>
+          <li>
+            <p>
+              auPay资产(USDT)
+              {/* <i className='iconfont icon-biyan'></i> */}
+              <i className='iconfont icon-chakan'></i>
+            </p>
+            <span>187,823.00</span>
+          </li>
+          <li>
+            <p>Oz资产(OZC)</p>
+            <span>3,092.00</span>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        最近一笔交易
+      </div>
+    </div>
   )
 }
 
