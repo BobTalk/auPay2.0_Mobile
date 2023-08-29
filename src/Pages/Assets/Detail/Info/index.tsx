4943ba73186c7cee38f9154036c856338eb0bddd
noahZhuD <51011311+noahZhuD@users.noreply.github.com>
2023-08-29 11:04:39 +0800

    资产详情状态页完成

--- src/Pages/Assets/Detail/Info/index.tsx
+++ src/Pages/Assets/Detail/Info/index.tsx
@@ -1,17 +1,23 @@
 import PublicHead from '@/Components/PublicHead'
 import { useState } from 'react'
-// import {  }
+import { ProgressBar } from 'antd-mobile'
 import './index.scss'
 
 const Info = () => {
-  const [ headData, setHeadData ] = useState({ title: '资产', back: 'goBack', textColor: 'white' })
+  // 根据传进来的数据判断是充币详情还是提币详情
+  const [ headData, setHeadData ] = useState({ title: 'USDT充币详情', back: 'goBack', textColor: 'white' })
 
   return (
     <div className='assets_info'>
       <PublicHead { ...headData } />
       <div className='assets_info_form'>
         <div className="assets_info_form_head">
-          <div className='assets_info_form_head_status'><i className="iconfont icon-top"></i></div>
+          <div className='assets_info_form_head_status'>
+            {/* 此处根据传进来的数据判断用哪个icon  进行中、成功、失败 */}
+            <i className="iconfont icon-top" />
+            {/* <i className="iconfont icon-chenggong" /> */}
+            {/* <i className="iconfont icon-shibai" /> */}
+          </div>
           <p><i className='iconfont icon-USDT' style={{ 'color' :'#17a37a' }}></i>87,823.00 USDT</p>
           <span>¥123,302.09</span>
         </div>
@@ -29,12 +35,17 @@ const Info = () => {
             <li><p>交易确认数</p><span><i className="assets_info_form_li_number">5</i>/6</span></li>
             <li>
               <p>交易状态</p>
-              <span>5/6</span>
+              <div className='assets_info_form_progress'>
+                {/* 进度条样式还要优化 */}
+                <ProgressBar percent={30} />
+                <span>进行中</span>
+              </div>
             </li>
           </ul>
         </div>
         
       </div>
+      <a className='goBrowser' href="" target='_blank'>到区块链浏览器查看交易详情</a>
     </div>
   )
 }
