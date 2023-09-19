40fb8c7e788ac18e8b206a0119b7c725d5abeeb2
noahZhuD <51011311+noahZhuD@users.noreply.github.com>
2023-09-19 14:49:48 +0800

    修改公告列表

--- src/Pages/Notice/index.tsx
+++ src/Pages/Notice/index.tsx
@@ -1,37 +1,41 @@
-import PublicHead from '@/Components/PublicHead'
-import './index.scss'
+import PublicHead from "@/Components/PublicHead";
+import styleScope from "./index.module.scss";
+import { HeadConfig } from "@/Assets/config/head";
+import { mergeClassName } from "@/utils/base";
 const Notice = () => {
-  const headData = { title: '公告列表', back: '/' }
+  let headData = Object.assign(HeadConfig, { title: "公告列表", back: "/" });
   return (
     <div>
-      <div className='public_w'>
-        <PublicHead { ...headData } />
+      <div className="px-[.3rem]">
+        <PublicHead {...headData} />
       </div>
-      <div className="notice_w public_w">
-        <ul className='notice_list'>
+      <div className={mergeClassName(styleScope["notice_w"],"px-[.3rem]")}>
+        <ul className={styleScope["notice_list"]}>
           <li>
-            <div className='notice_list_tit'>
+            <div className={styleScope["notice_list_tit"]}>
               <p>转账记录通知</p>
               <i>2023-7-19</i>
             </div>
-            <span>尊敬的客户：您好！为向您提供更优质的服务，我司对感谢您一直以来的支持和理解，我们会不断提高业务的专业性尊敬的客户：您好！为向您提供更优质的服务，我司对感谢您一直以来的支持和理解，我们会不断提高业务的专业性。</span>
+            <span>
+              尊敬的客户：您好！为向您提供更优质的服务，我司对感谢您一直以来的支持和理解，我们会不断提高业务的专业性尊敬的客户：您好！为向您提供更优质的服务，我司对感谢您一直以来的支持和理解，我们会不断提高业务的专业性。
+            </span>
           </li>
           <li>
-            <div className='notice_list_tit'>
-              <p>转账记录通知</p>
-              <i>2023-7-19</i>
-            </div>
-            <span>尊敬的客户：您好！为向您提供更优质的服务，我对…</span>
-          </li>
-          <li>
-            <div className='notice_list_tit'>
+            <div className={styleScope["notice_list_tit"]}>
               <p>转账记录通知</p>
               <i>2023-7-19</i>
             </div>
             <span>尊敬的客户：您好！为向您提供更优质的服务，我对…</span>
           </li>
           <li>
-            <div className='notice_list_tit'>
+            <div className={styleScope["notice_list_tit"]}>
+              <p>转账记录通知</p>
+              <i>2023-7-19</i>
+            </div>
+            <span>尊敬的客户：您好！为向您提供更优质的服务，我对…</span>
+          </li>
+          <li>
+            <div className={styleScope["notice_list_tit"]}>
               <p>转账记录通知</p>
               <i>2023-7-19</i>
             </div>
@@ -40,6 +44,6 @@ const Notice = () => {
         </ul>
       </div>
     </div>
-  )
-}
-export default Notice
+  );
+};
+export default Notice;
