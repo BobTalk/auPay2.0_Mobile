5eced14878520a8d373513d85e1a6ccaf4f184d4
noahZhuD <51011311+noahZhuD@users.noreply.github.com>
2023-10-09 18:09:57 +0800

    app管理

--- src/Pages/My/APPManager/index.tsx
+++ src/Pages/My/APPManager/index.tsx
@@ -1,13 +1,17 @@
 import PublicHead from "@/Components/PublicHead";
 import { Avatar } from "antd-mobile";
-import { MouseEvent, useRef } from "react";
+import { MouseEvent, useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { InfoSecurity } from "../../Enum";
 import { HeadConfig } from "@/Assets/config/head";
+import { GetUserApplyApplication } from "@/Api";
+import appLogo from '../../../Assets/images/app_logo.png'
+import styleScope from "./index.module.scss";
 
 const AppManager = (props: any) => {
   const HeaderEl = useRef();
   const Navigate = useNavigate();
+  let [pageList, usePageList] = useState([]);
   let { state: urlParams }: any = useLocation();
   let headInfo = Object.assign(HeadConfig, {
     title: props.headTitle ?? urlParams?.headTitle,
@@ -25,32 +29,46 @@ const AppManager = (props: any) => {
       },
     });
   }
-
+  function getPageInfo() {
+    GetUserApplyApplication().then((res) => {
+      usePageList(res.value);
+    });
+  }
+  useEffect(() => {
+    getPageInfo();
+  }, []);
   return (
     <>
       <PublicHead {...headInfo} ref={HeaderEl} />
       <ul className="mx-[.3rem]">
-        <li className="flex py-[.3rem] justify-between border-b-[#DBDBDB] border-b-[1px]">
-          <div className="flex">
-            <Avatar
-              src=""
-              style={{ "--size": ".8rem", "--border-radius": ".16rem" }}
-              className="aspect-square mr-[.24rem]"
-            />
-            <p className="flex flex-col justify-center">
-              <span className="text-[.3rem] text-[#333] font-[700]">Ozbet</span>
-              <span className="text-[.26rem] text-[#333]">
-                用户名：嘻嘻的嘻嘻
-              </span>
-            </p>
-          </div>
-          <span
-            onClick={(e) => unBind(e, {})}
-            className="text-[.26rem] text-[#63717B] flex items-end"
+        {pageList?.map((item: any) => (
+          <li
+            key={item.applicaitonId}
+            className="flex py-[.3rem] justify-between border-b-[#DBDBDB] border-b-[1px]"
           >
-            解除绑定
-          </span>
-        </li>
+            <div className="flex">
+              <Avatar
+                src={appLogo}
+                style={{ "--size": ".8rem", "--border-radius": ".16rem" }}
+                className={styleScope["aspect-square"]+" "+"mr-[.24rem]"}
+              />
+              <p className="flex flex-col justify-center">
+                <span className="text-[.3rem] text-[#333] font-[700]">
+                  {item.applicaitonName ?? "--"}
+                </span>
+                <span className="text-[.26rem] text-[#333]">
+                  用户名：{item.applicationUsername}
+                </span>
+              </p>
+            </div>
+            <span
+              onClick={(e) => unBind(e, item)}
+              className="text-[.26rem] text-[#63717B] flex items-end"
+            >
+              解除绑定
+            </span>
+          </li>
+        ))}
       </ul>
     </>
   );
