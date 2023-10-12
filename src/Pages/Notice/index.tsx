bb19adfe28bff33b7bff3eef2034c3ffdbf0fe55
noahZhuD <51011311+noahZhuD@users.noreply.github.com>
2023-10-12 14:37:56 +0800

    fix

--- src/Pages/Notice/index.tsx
+++ src/Pages/Notice/index.tsx
@@ -2,14 +2,18 @@ import PublicHead from "@/Components/PublicHead";
 import styleScope from "./index.module.scss";
 import { HeadConfig } from "@/Assets/config/head";
 import { mergeClassName, timeFormate } from "@/utils/base";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { ViewAnnouncement } from "@/Api";
 import { Ellipsis, InfiniteScroll } from "antd-mobile";
+import PublicScroll from "@/Components/PublicScroll";
 const Notice = () => {
   let headData = Object.assign(HeadConfig, {
     title: "公告列表",
     back: "goBack",
+    className: "p-[.32rem_.3rem] text-[#333]",
   });
+  const JHeaderRef = useRef<any>({});
+  const [contentHeight, setContentHeight] = useState(0);
   let [pagination, setPagination] = useState({
     pageNo: 1,
     pageSize: 10,
@@ -19,33 +23,43 @@ const Notice = () => {
     let notice = await ViewAnnouncement(pagination);
     setNotice(notice);
   }
-
+  useEffect(() => {
+    let {height} = JHeaderRef.current.getBoundingClientRect();
+    let contentH = window.innerHeight;
+    console.log("contentH: ", contentH);
+    setContentHeight(contentH - height);
+  }, []);
   useEffect(() => {
     InitPageInfo();
   }, []);
   return (
-    <div>
-      <div className="px-[.3rem]">
-        <PublicHead {...headData} />
-      </div>
-      <div className={mergeClassName(styleScope["notice_w"], "px-[.3rem]")}>
+    <PublicScroll>
+      <PublicHead {...headData} ref={JHeaderRef} />
+      <div
+        style={{
+          height: `calc(${contentHeight}px)`,
+        }}
+        className={mergeClassName(styleScope["notice_w"], "px-[.3rem]")}
+      >
         <ul className={styleScope["notice_list"]}>
           <ItemComp list={notice?.data} />
+          {pagination.pageNo < notice.pageTotal ? (
+            <InfiniteScroll
+              hasMore={pagination.pageNo < notice.pageTotal}
+              loadMore={(isRetry: boolean): any => {
+                if (isRetry) {
+                  setPagination({
+                    ...pagination,
+                    pageNo: ++pagination.pageNo,
+                  });
+                }
+              }}
+              threshold={35}
+            ></InfiniteScroll>
+          ) : null}
         </ul>
       </div>
-      <InfiniteScroll
-        hasMore={pagination.pageNo < notice.pageTotal}
-        loadMore={(isRetry: boolean): any => {
-          if (isRetry) {
-            setPagination({
-              ...pagination,
-              pageNo: ++pagination.pageNo,
-            });
-          }
-        }}
-        threshold={35}
-      ></InfiniteScroll>
-    </div>
+    </PublicScroll>
   );
 };
 const ItemComp = ({ list }: any) =>
